a6debb2a0e89248f1a34f463faae1c5a79565e09
stanley <stanley.njuguna@student.moringaschool.com>
2024-01-15 03:27:50 +0300

    complete assignment

--- main.js
+++ main.js
@@ -3,6 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const parentdiv=document.getElementById('modal')
+const h2element=parentdiv.querySelector('h2')
+parentdiv.classList.add('hidden')
+
+
+const likeButton =document.querySelectorAll('.like-glyph');
+likeButton.forEach ((buton)=>{
+  buton.addEventListener('click',(e)=>{
+ mimicServerCall()
+
+   .then((data) =>{
+   if (buton.textContent === EMPTY_HEART){
+    buton.textContent=FULL_HEART ;
+    buton.classList.add('activated-heart')
+   }else{
+    buton.textContent=EMPTY_HEART ;
+    buton.classList.remove('activated-heart')
+   }
+  })
+   .catch(error => {
+    parentdiv.classList.remove('hidden');
+    h2element.textContent=error
+
+    setTimeout(function(){
+      parentdiv.classList.add('hidden');
+
+   },1000);
+  })
+    })
+
+
+})
 
 
 
